e4ecc8fc507fe6fa059cf482ac20f793f9f164e6
Kritika Pathak <55829077+kritika2311@users.noreply.github.com>
2022-07-14 03:15:33 +0530

    Create ARRAY_IMPL1.java

---
+++ Arrays/ARRAY_IMPL1.java
@@ -0,0 +1,20 @@
+ArrayList<Integer> performOps(ArrayList<Integer> A) {
+        ArrayList<Integer> B = new ArrayList<Integer>();
+        for (int i = 0; i < 2 * A.size(); i++) B.add(0);
+        for (int i = 0; i < A.size(); i++) {
+                B.set(i, A.get(i));
+                B.set(i + A.size(), A.get((A.size() - i) % A.size()));
+        }
+        return B;
+}
+
+/*Lets say performOps was called with A : [5, 10, 2, 1].
+What would be the output of the following call :*/
+
+
+ArrayList<Integer> B = performOps(A);
+for (int i = 0; i < B.size(); i++) {
+        System.out.print(B.get(i) + " ");
+}
+
+//Output: 5 10 2 1 5 1 2 10
